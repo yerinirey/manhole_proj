604b4ce4cff3a39216f040b16dd74a0ad48e2584
yerinirey <101080932+yerinirey@users.noreply.github.com>
2023-09-14 01:05:35 +0900

    Add files via upload

--- GoYangYang/main.py
+++ GoYangYang/main.py
@@ -1,43 +1,54 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO
 import serial
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 PORT = "COM8"
 BaudRate = 9600
-ARD = serial.Serial(PORT, BaudRate)
 db = []
-
-def Decode(response):
-    response = response[:len(response) - 2].decode() # 문자열 끝에 붙는 \r\n 슬라이싱
-    state, accelX, accelY, accelZ = response.split(',')
-    # ['LIGHT ON/off", 'a.acceleration.x', 'a.acceleration.y', 'a.acceleration.z']
-    if state == "LIGHT ON":
-        return f"기울임이 감지되었습니다: {accelX, accelY, accelZ}"
-    elif state == "off":
-        return ". . ."
-    else:
-        return "Wrong access from _Decode_"
-
-def Ardread(ARD):
-    if ARD.readable():
-        LINE = ARD.readline()
-        code = Decode(LINE)
-        print(code)
-        return code
-    else:
-       return "읽기 실패 from _Ardread_"       
-
-
 
 @app.route('/')
 def home():
-    code = Ardread(ARD)
-    db.append(code)
+    # ARD = serial.Serial(PORT, BaudRate)
+    # LINE = ARD.readline()
+    # state, accelX, accelY, accelZ = LINE[:len(LINE) - 2].decode("utf-8").split(',')
+    # if state == "LIGHT ON":
+    #     message = f"기울임이 감지되었습니다: {accelX, accelY, accelZ}"
+    # elif state == "off":
+    #     message = ". . ."
+    # else:
+    #     message = "Wrong access from _Decode_"
+    # # code = Ardread(ARD)
+    db.append(message)
     return render_template("home.html", DataHtml=db)
+
+@socketio.on('connect')
+def handle_connect():
+    print("웹 소켓 연결")
+    
+@socketio.on("disconnect")
+def handle_disconnect():
+    print("웹 소켓 연결 해제")
+
+@socketio.on("get_sensor_data")
+def get_sensor_data():
+    ARD = serial.Serial(PORT, BaudRate)
+    LINE = ARD.readline()
+    state, accelX, accelY, accelZ = LINE[:len(LINE) - 2].decode("utf-8").split(',')
+    if state == "LIGHT ON":
+        message = f"기울임이 감지되었습니다: {accelX, accelY, accelZ}"
+    elif state == "off":
+        message = ". . ."
+    else:
+        message = "Wrong access from _Decode_"
+    sensor_data = message
+    socketio.emit('sensor_data', sensor_data)
+    
 
 @app.route('/location')
 def location():
     return render_template("location.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
